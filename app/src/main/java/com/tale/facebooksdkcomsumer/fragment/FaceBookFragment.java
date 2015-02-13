103a7a7ada41d30079aa02d0eb7cca7672b3e935
Giang Nguyễn <giang@gkxim.com>
2015-02-13 11:29:19 +0700

    !update add publish story

--- app/src/main/java/com/tale/facebooksdkcomsumer/fragment/FaceBookFragment.java
+++ app/src/main/java/com/tale/facebooksdkcomsumer/fragment/FaceBookFragment.java
@@ -3,25 +3,60 @@ package com.tale.facebooksdkcomsumer.fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 
 import com.facebook.FacebookOperationCanceledException;
+import com.facebook.FacebookRequestError;
+import com.facebook.HttpMethod;
+import com.facebook.Request;
+import com.facebook.RequestAsyncTask;
+import com.facebook.Response;
 import com.facebook.Session;
 import com.facebook.SessionState;
 import com.facebook.UiLifecycleHelper;
 import com.facebook.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * Created by giang on 1/14/15.
  */
 public abstract class FaceBookFragment extends Fragment {
 
+    private final String TAG;
     protected UiLifecycleHelper uiHelper;
+    private LoginButton loginButton;
+    private String[] permissionsString;
+
+    private static final List<String> PERMISSIONS = Arrays.asList("publish_actions");
+    private static final String PENDING_PUBLISH_KEY = "pendingPublishReauthorization";
+    private boolean pendingPublishReauthorization = false;
+    private String name;
+    private String caption;
+    private String description;
+    private String link;
+    private String picture;
+
+    FaceBookFragment() {
+        TAG = getClass().getSimpleName();
+    }
 
     public static enum ReadPermission {
         email,
         public_profile,
         user_friends,
         user_likes,
+        user_birthday,
+        user_location
+    }
+
+    public static enum PublishPermission {
+        publish_actions,
     }
 
     private Session.StatusCallback callback = new Session.StatusCallback() {
@@ -31,21 +66,44 @@ public abstract class FaceBookFragment extends Fragment {
                 onUserCancelLogin(session, state, exception);
             } else if (state.isOpened()) {
                 onLoginSuccess(session, state);
+            } else if (state.isClosed()) {
+                onLogout(session, state);
             } else {
                 onSessionStateChange(session, state, exception);
             }
         }
     };
 
-    public void setReadPermissions(LoginButton loginButton, ReadPermission... permissions) {
+    public void takeLoginButton(LoginButton loginButton) {
+        this.loginButton = loginButton;
+        loginButton.setFragment(this);
+    }
+
+    public void login() {
+        Session session = Session.getActiveSession();
+        if (session != null && !session.isOpened() && !session.isClosed()) {
+            final Session.OpenRequest openRequest = new Session.OpenRequest(this)
+                    .setCallback(callback);
+            if (permissionsString != null) {
+                openRequest.setPermissions(permissionsString);
+            }
+            session.openForRead(openRequest);
+        } else {
+            Session.openActiveSession(getActivity().getApplicationContext(), this, true, callback);
+        }
+    }
+
+    public void setReadPermissions(ReadPermission... permissions) {
         if (permissions == null || permissions.length == 0) {
             return;
         }
-        final String[] permissionsString = new String[permissions.length];
+        this.permissionsString = new String[permissions.length];
         for (int i = 0; i < permissions.length; i++) {
-            permissionsString[i] = permissions[i].toString();
+            this.permissionsString[i] = permissions[i].toString();
         }
-        loginButton.setReadPermissions(permissionsString);
+        if (loginButton != null) {
+            loginButton.setReadPermissions(this.permissionsString);
+        }
     }
 
     @Override
@@ -53,8 +111,11 @@ public abstract class FaceBookFragment extends Fragment {
         super.onCreate(savedInstanceState);
         uiHelper = new UiLifecycleHelper(getActivity(), callback);
         uiHelper.onCreate(savedInstanceState);
+        if (savedInstanceState != null) {
+            pendingPublishReauthorization =
+                    savedInstanceState.getBoolean(PENDING_PUBLISH_KEY, false);
+        }
     }
-
 
     @Override
     public void onResume() {
@@ -64,7 +125,7 @@ public abstract class FaceBookFragment extends Fragment {
         // may not be triggered. Trigger it if it's open/closed.
         final Session session = Session.getActiveSession();
         if (session != null &&
-                (session.isOpened() || session.isClosed()) ) {
+                (session.isOpened() || session.isClosed())) {
             onSessionStateChange(session, session.getState(), null);
         }
         uiHelper.onResume();
@@ -91,17 +152,105 @@ public abstract class FaceBookFragment extends Fragment {
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
+        outState.putBoolean(PENDING_PUBLISH_KEY, pendingPublishReauthorization);
         uiHelper.onSaveInstanceState(outState);
     }
 
     protected void onLoginSuccess(Session session, SessionState state) {
+        Session.setActiveSession(session);
+    }
+
+    protected void onLogout(Session session, SessionState state) {
 
     }
 
     protected void onUserCancelLogin(Session session, SessionState state, Exception exception) {
+
     }
 
     protected void onSessionStateChange(Session session, SessionState state, Exception exception) {
+        if (pendingPublishReauthorization &&
+                state.equals(SessionState.OPENED_TOKEN_UPDATED)) {
+            pendingPublishReauthorization = false;
+            publishPendingStory();
+        }
     }
 
+    private void publishPendingStory() {
+        publishStory(name, caption, description, link, picture);
+    }
+
+    protected void publishStory(String name, String caption, String description, String link, String picture) {
+        this.name = name;
+        this.caption = caption;
+        this.description = description;
+        this.link = link;
+        this.picture = picture;
+        Session session = Session.getActiveSession();
+
+        if (session != null) {
+
+            // Check for publish permissions
+            List<String> permissions = session.getPermissions();
+            if (!isSubsetOf(PERMISSIONS, permissions)) {
+                pendingPublishReauthorization = true;
+                Session.NewPermissionsRequest newPermissionsRequest = new Session
+                        .NewPermissionsRequest(this, PERMISSIONS);
+                session.requestNewPublishPermissions(newPermissionsRequest);
+                return;
+            }
+
+            Bundle postParams = new Bundle();
+            postParams.putString("name", name);
+            postParams.putString("caption", caption);
+            postParams.putString("description", description);
+            postParams.putString("link", link);
+            postParams.putString("picture", picture);
+
+            Request.Callback callback = new Request.Callback() {
+                public void onCompleted(Response response) {
+                    JSONObject graphResponse = response
+                            .getGraphObject()
+                            .getInnerJSONObject();
+                    String postId = null;
+                    try {
+                        postId = graphResponse.getString("id");
+                    } catch (JSONException e) {
+                        Log.i(TAG,
+                                "JSON error " + e.getMessage());
+                    }
+                    FacebookRequestError error = response.getError();
+                    if (error != null) {
+                        onPublishStoryError(error);
+                    } else {
+                        onPublishStoreSuccess(postId);
+                    }
+                }
+            };
+
+            Request request = new Request(session, "me/feed", postParams,
+                    HttpMethod.POST, callback);
+
+            RequestAsyncTask task = new RequestAsyncTask(request);
+            task.execute();
+        }
+
+    }
+
+    private boolean isSubsetOf(Collection<String> subset, Collection<String> superset) {
+        for (String string : subset) {
+            if (!superset.contains(string)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    protected void onPublishStoreSuccess(String postId) {
+
+    }
+
+    protected void onPublishStoryError(FacebookRequestError error) {
+
+    }
 }
--- app/src/main/java/com/tale/facebooksdkcomsumer/fragment/LoginFragment.java
+++ app/src/main/java/com/tale/facebooksdkcomsumer/fragment/LoginFragment.java
@@ -7,8 +7,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.facebook.HttpMethod;
+import com.facebook.Request;
+import com.facebook.Response;
 import com.facebook.Session;
 import com.facebook.SessionState;
+import com.facebook.model.GraphUser;
 import com.facebook.widget.LoginButton;
 import com.tale.facebooksdkcomsumer.R;
 
@@ -30,8 +34,37 @@ public class LoginFragment extends FaceBookFragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         LoginButton authButton = (LoginButton) view.findViewById(R.id.authButton);
-        setReadPermissions(authButton, ReadPermission.email, ReadPermission.user_likes);
-        authButton.setFragment(this);
+        takeLoginButton(authButton);
+        setReadPermissions(ReadPermission.email, ReadPermission.user_likes, ReadPermission.user_birthday);
+        view.findViewById(R.id.btManualLogin).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                login();
+            }
+        });
+        view.findViewById(R.id.btManualLogout).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                logout();
+            }
+        });
+    }
+
+    private void logout() {
+        if (Session.getActiveSession() != null) {
+            Session.getActiveSession().closeAndClearTokenInformation();
+        }
+
+        Session.setActiveSession(null);
+    }
+
+    @Override protected void onLoginSuccess(Session session, SessionState state) {
+        super.onLoginSuccess(session, state);
+        Log.i(TAG, "Logged in... => state: " + state + " session: " + session.getAccessToken());
+        Request.newMeRequest(session, new Request.GraphUserCallback() {
+            @Override public void onCompleted(GraphUser graphUser, Response response) {
+                final String birthday = graphUser.getBirthday();
+                Log.i(TAG, "Birth day => state: " + birthday);
+            }
+        }).executeAsync();
     }
 
     @Override
@@ -40,19 +73,24 @@ public class LoginFragment extends FaceBookFragment {
         Log.i(TAG, "User cancel login");
     }
 
-    @Override protected void onSessionStateChange(Session session, SessionState state, Exception exception) {
-        super.onSessionStateChange(session, state, exception);
-        if (state.isOpened()) {
-            List<String> permissions = session.getPermissions();
-            if (permissions != null && permissions.size() > 0) {
-                for (String permission : permissions) {
-                    Log.i(TAG, "Permission: " + permission.toString());
-                }
-            }
-            Log.i(TAG, "Logged in... => exception " + exception + " state: " + state + "session: " + session.getAccessToken());
-        } else if (state.isClosed()) {
-            Log.i(TAG, "Logged out... => exception " + exception + " state: " + state + "session: " + session.getAccessToken());
-        }
+    @Override protected void onLogout(Session session, SessionState state) {
+        super.onLogout(session, state);
+        Log.i(TAG, "Logged out... => state: " + state + " session: " + session.getAccessToken());
     }
+
+    //    @Override protected void onSessionStateChange(Session session, SessionState state, Exception exception) {
+//        super.onSessionStateChange(session, state, exception);
+//        if (state.isOpened()) {
+//            List<String> permissions = session.getPermissions();
+//            if (permissions != null && permissions.size() > 0) {
+//                for (String permission : permissions) {
+//                    Log.i(TAG, "Permission: " + permission.toString());
+//                }
+//            }
+//            Log.i(TAG, "Logged in... => exception " + exception + " state: " + state + "session: " + session.getAccessToken());
+//        } else if (state.isClosed()) {
+//            Log.i(TAG, "Logged out... => exception " + exception + " state: " + state + "session: " + session.getAccessToken());
+//        }
+//    }
 
 }
